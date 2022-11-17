2096698703d2da0a93397fd8a9c693af02691d0e
Ashia <79714747+Ashui-Idiot@users.noreply.github.com>
2022-11-17 13:39:36 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 function go() {
+  if (!document.getElementById("link").value.includes("https://") || !document.getElementById("link").value.includes("http://") {
+    document.getElementById("link").value = "http://" + document.getElementById("link").value;    
+  };
   document.getElementById("iframe").src=document.getElementById("link").value;
 }
